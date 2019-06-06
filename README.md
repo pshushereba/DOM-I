8e28d079c65461d0d6c01585d6b29f9f32d287bc
Patrick Shushereba <pshushereba@gmail.com>
2019-06-05 19:36:25 -0700

    Styled nav and main content areas.

--- README.md
+++ README.md
@@ -12,11 +12,11 @@ You have been provided a [JSON object](js/index.js) with all the necessary data 
 **Follow these steps to set up and work on your project:**
 
 * [x] Create a forked copy of this project.
-* [ ] Add your project manager as collaborator on Github.
+* [x] Add your project manager as collaborator on Github.
 * [x] Clone your OWN version of the repository (Not Lambda's by mistake!).
 * [x] Create a new branch: git checkout -b `<firstName-lastName>`.
 * [x] Implement the project on your newly created `<firstName-lastName>` branch, committing changes regularly.
-* [ ] Push commits: git push origin `<firstName-lastName>`.
+* [x] Push commits: git push origin `<firstName-lastName>`.
 
 **Follow these steps for completing your project.**
 
@@ -25,8 +25,8 @@ You have been provided a [JSON object](js/index.js) with all the necessary data 
 * [ ] Your project manager will count the project as complete by merging the branch back into master.
 
 ## Task 1: Create selectors to point your data into elements
-* [ ] Create selectors by using any of the DOM element's methods
-* [ ] Note that IDs have been used on all images.  Use the IDs to update src path content
+* [x] Create selectors by using any of the DOM element's methods
+* [x] Note that IDs have been used on all images.  Use the IDs to update src path content
 
 ## Task 2: Update the HTML with the JSON data
 * [ ] Remember, NO direct updating of the HTML source is allowed.
--- js/index.js
+++ js/index.js
@@ -37,6 +37,66 @@ const siteContent = {
   },
 };
 
+
+
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+links = document.querySelectorAll("nav a");
+links.forEach((item, i) => {
+  item.textContent = siteContent["nav"][`nav-item-${i+1}`];
+});
+
+// CTA styling
+
+let ctaText = document.querySelector(".cta-text h1");
+ctaText.textContent = siteContent["cta"]["h1"];
+
+let ctaLogo = document.getElementById("cta-img");
+ctaLogo.src = siteContent["cta"]["img-src"];
+
+let ctaBtn = document.querySelector(".cta-text button");
+ctaBtn.textContent = siteContent["cta"]["button"];
+
+// Main Content styling
+
+const tcTitles = document.querySelectorAll(".top-content .text-content h4");
+const tcContent = document.querySelectorAll(".top-content .text-content p");
+
+
+const featuresTitle = tcTitles[0];
+featuresTitle.textContent = siteContent["main-content"]["features-h4"];
+
+const featuresContent = tcContent[0];
+featuresContent.textContent = siteContent["main-content"]["features-content"];
+
+const about = tcTitles[1];
+about.textContent = siteContent["main-content"]["about-h4"];
+
+const aboutContent = tcContent[1];
+aboutContent.textContent = siteContent["main-content"]["about-content"];
+
+let midImg = document.querySelector(".middle-img");
+midImg.src = siteContent["main-content"]["middle-img-src"];
+
+const bcTitles = document.querySelectorAll(".bottom-content .text-content h4");
+const bcContent = document.querySelectorAll(".bottom-content .text-content p");
+
+const servicesTitle = bcTitles[0];
+servicesTitle.textContent = siteContent["main-content"]["services-h4"];
+
+const servicesContent = bcContent[0];
+servicesContent.textContent = siteContent["main-content"]["services-content"];
+
+const productTitle = bcTitles[1];
+productTitle.textContent = siteContent["main-content"]["product-h4"];
+
+const productContent = bcContent[1];
+productContent.textContent = siteContent["main-content"]["product-content"];
+
+const visionTitle = bcTitles[2];
+visionTitle.textContent = siteContent["main-content"]["vision-h4"];
+
+const visionContent = bcContent[2];
+visionContent.textContent = siteContent["main-content"]["vision-content"];
